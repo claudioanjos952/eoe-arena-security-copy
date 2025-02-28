5c33e8ffeacf7df3c0268dbdeba17a70a65843ba
claudioanjos952 <claudioanjos952@gmail.com>
2025-02-28 02:23:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -166,12 +166,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("sample_mflix").command({ ping: 1 });
-	  const SERVER.db = client.db("sample_mflix");
+	 return SERVER.db = client.db("sample_mflix");
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  } 
 }
 run().catch(console.dir);
   // Socket.io init
